47b100014c728fc84d9de2c30eb614e92553fb98
zzoo <zzoo.origin@gmail.com>
2025-07-23 00:09:57 +0900

    Add path mapping for theme and i18n, enhance App component with translations, and update CSS for theme-specific button styles

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@theme': path.resolve(__dirname, './@theme/src'),
+      '@i18n': path.resolve(__dirname, './@i18n/src'),
+    },
+  },
 })
